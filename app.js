a4383db32646647c1c1444658c7fe6d2af574c3b
Hamzabaloch08 <hb.developer08@gmail.com>
2024-02-19 03:24:33 +0500

    Javascript practice

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert("hi there i'm hamza baloch")
+// () <- (this is a function) --> alert is a Built-in Function that is throw a pop up in your website
+// 2 types of function in javascript
+// (1st function is Built-in Function) -->  which are built in java script
+// (2nd function is Manual / User define Function) --> created by user
+
+// DATA TYPE
+// 3 types of DATA TYPE
+
+// 1st is STRING:
+// ("" --> '') something come inside single quotation or double quotation is called string  and what is string ? string is data type of java script.
+
+// 2nd is INTEGER:
+// example: 1, 2, 3, 4, 5, 123456 this is INTEGER
+
+// 3rd is FLOAT
+// example: 1.1, 2.3, 6.4 whenever we talk about point is called FLOAT.
+
+// chapter variable for string
+
+
