deedf42991667066d118a476e2363befdda4e3c2
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-15 21:13:23 +0500

    Prompt practice

--- 3-day/app.js
+++ 3-day/app.js
@@ -2,13 +2,13 @@ var num1 = +prompt("Enter anynumber");
 var num2 = +prompt("Enter another number");
 
 var add = num1 + num2;
-document.write(add + "Add" + "<br>");
+document.write(add + "<b>" + " Add" + "</b>" + "<br>");
 
 var sub = num1 - num2;
-document.write(sub + "Subtract" + "<br>");
+document.write(sub + "<b>" + " Subtract" + "</b>" + "<br>");
 
 var mul = num1 * num2;
-document.write(mul + "Multifly" + "<br>");
+document.write(mul + "<b>" + " Multifly" + "</b>" + "<br>");
 
 var mod = num1 % num2;
-document.write(mod + "Modules");
+document.write(mod + "<b>" + " Modules" + "</b>");
