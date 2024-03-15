dcea3f5c9f897803c7bc32581893aa33185e165b
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-15 20:45:49 +0500

    practice

--- 3-day/app.js
+++ 3-day/app.js
@@ -2,13 +2,13 @@ var num1 = +prompt("Enter anynumber");
 var num2 = +prompt("Enter another number");
 
 var add = num1 + num2;
-document.write("<h2>" + add + "</h2>" + "<h1>" + + "</h1>");
+document.write("<h2>" + add + "</h2>" + "<h1>" + "Add" + "</h1>");
 
 var sub = num1 - num2;
-document.write("<h2>" + sub + "</h2>" + "Subtract" + "<h1>" + + "</h1>");
+document.write("<h2>" + sub + "</h2>" + "<h1>" + "Subtract" + "</h1>");
 
 var mul = num1 * num2;
-document.write("<h2>" + mul + "</h2>" + "Multiply" + "<h1>" + + "</h1>");
+document.write("<h2>" + mul + "</h2>" + "<h1>" + "Multiply" + "</h1>");
 
 var mod = num1 % num2;
 document.write("<h2>" + mod + "</h2>" + "<h1>" +  "Modulas" + "</h1>");
