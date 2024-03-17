5d8b3bd5137c4fae683bc3832b89dcb6051edc24
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-17 10:43:11 +0500

    learn array

---
+++ 5-day/app.js
@@ -0,0 +1,46 @@
+// var array = ["hamza", "baloch", 16, true];
+
+// document.write(array[0] + " " + array[1]);
+
+
+
+// var array = [6, 5, 4, 3, 2];
+// array.push(1, 0); //built-in function
+// array.pop(); //built-in function
+// array.pop();
+
+// document.write(array);
+
+
+
+
+// var array = [2,3,4,5,6]
+// array.unshift(0,1)
+// array.shift()
+
+// document.write(array)
+
+
+
+
+// var array = [1, 2, 3, 4, 5];
+// array.splice(3,2,"hello world","js");
+
+// document.write(array);
+
+
+
+// var arr = [1, 2, 3, 4, 5, "abc"];
+// var arr2 = arr.slice(0);
+
+// document.write(arr2);
+
+
+
+
+//.push () adds one or more elements to the end of
+//.pop () removes the last element from an array and returns it
+//.shift () removes the first element from an array and returns it
+//.unshift() adds one or more elements to the beginning of an array
+//.splice() changes the content of an array, can add or delete  elements
+// .slice()  method extracts a section of an array and returns new array
