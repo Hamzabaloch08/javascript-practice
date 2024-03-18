53181327c516a7db14864bcb98feee2b34734969
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-19 02:50:45 +0500

    DAY 14 OF LEARNING OF JAVASCRIPT

---
+++ 6-day/app.js
@@ -0,0 +1,17 @@
+// day 14 of learning of JAVASCRIPT
+
+var userData = +prompt("Enter your age");
+
+if (userData < 18) {
+  document.write("<h1>" + "You're under age for this Program." + "<h1>");
+} else if (userData >= 18 && userData <= 30) {
+  document.write(
+    "<h1>" + "Congratulations You're Eligible for this program" + "<h1>"
+  );
+} else if (userData > 30 && userData <= 80) {
+  document.write("<h1>" + "You're over age for this Program." + "<h1>");
+} else if (userData > 80) {
+  document.write("<h1>" + "You are still alive" + "<h1>");
+} else {
+  alert("please enter your age");
+}
