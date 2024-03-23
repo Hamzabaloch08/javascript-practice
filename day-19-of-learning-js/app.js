524345f9a3a4cc80c1fd68ff6126fe934734eb0a
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-23 20:42:53 +0500

    day-19-of-learning-js

---
+++ day-19-of-learning-js/app.js
@@ -0,0 +1,13 @@
+// chapter for loop
+// there are 3 basics loops
+// 1: for loop
+// 2: while loop
+// 3: do-while loop
+
+// for loop
+
+// var i;
+
+// for (i = 10; i >= 0; i--) {
+//   document.write(i + "<br>");
+// }
