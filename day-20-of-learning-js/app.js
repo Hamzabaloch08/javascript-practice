bb7466b23eed969842f8eeff7ccb383b026cb50b
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-25 07:44:59 +0500

    day 20 of learning js

--- day-20-of-learning-js/app.js
+++ day-20-of-learning-js/app.js
@@ -4,11 +4,15 @@
 //   document.write(array[i] + "<br>");
 // }
 
-
-
 // var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 // for (var i = 0; i < array.length; i++) {
 //   document.write(array[i] + "<br>");
 // }
 
+var userInput = +prompt("Which table you want");
+var userInput2 = +prompt("1 TILL ?");
+
+for (var i = 1; i <= userInput2; i++) {
+  document.write(userInput + " x " + i + " = " + userInput * i + "<br>");
+}
