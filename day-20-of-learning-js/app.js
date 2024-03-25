8ca444a28c8e9a19dd0c8492de3700a7a30bee4b
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-25 07:28:00 +0500

    day 20

---
+++ day-20-of-learning-js/app.js
@@ -0,0 +1,13 @@
+// var array = [0,1,2,3,4,5,6,7,8,9];
+
+// for (var i = 0; i < 10; i++) {
+//   document.write(array[i] + "<br>");
+// }
+
+
+
+var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+for (var i = 0; i < array.length; i++) {
+  document.write(array[i] + "<br>");
+}
