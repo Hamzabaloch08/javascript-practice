68e6d27fb923bde3e5968b8184ae75efe273e7ae
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-26 09:29:43 +0500

    day 21 of learning js

--- day-21-of-learning-js/app.js
+++ day-21-of-learning-js/app.js
@@ -10,7 +10,14 @@
 //     }
 // }
 // if(match == false){
-//    alert("City not found") 
+//    alert("City not found")
 // }
 
+var arr = ["salman", "arbaaz", "noman"];
+var arr2 = ["farhan", "zafar"];
 
+for (var i = 0; i < arr.length; i++) {
+    for(var i = 0; i < arr.length; i++ ){
+
+    }
+}
