9b4e453d50363a661924dd05a30d78a2975941b1
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-26 09:49:45 +0500

    today i learn how to use if else on loop and nested for loop

--- day-21-of-learning-js/app.js
+++ day-21-of-learning-js/app.js
@@ -1,5 +1,7 @@
 // . for loops: flags, Booleans, array length, and breaks
 
+
+
 // var userinput = prompt("Enter city name");
 // var cityNames = ["karachi", "lahore", "islamabad", "peshawer", "quetta"];
 // var match = false;
@@ -15,13 +17,15 @@
 //    alert("City not found")
 // }
 
+
 // for loop nasted
 
-var arr = ["salman", "arbaaz", "noman"];
-var arr2 = ["farhan", "zafar"];
 
-for (var i = 0; i < arr.length; i++) {
-  for (var j = 0; i < arr2.length; j++) {
-    document.write(arr[i] + " " + arr2[j] + "<br>");
-  }
-}
+// var arr = ["salman", "arbaaz", "noman"];
+// var arr2 = ["farhan", "zafar"];
+
+// for (var i = 0; i < arr.length; i++) {
+//     for(var j = 0; i < arr2.length; j++ ){
+//         document.write(arr[i] + " " + arr2[j] +"<br>");
+//     }
+// }
