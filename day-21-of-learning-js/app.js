54a32b1a2dda71b1599e3d5ab1e8cb3f7e01e329
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-26 08:59:37 +0500

    today i learn how to use if else on for loop

--- day-21-of-learning-js/app.js
+++ day-21-of-learning-js/app.js
@@ -1,16 +1,16 @@
-var userinput = prompt("Enter city name");
-var cityNames = ["karachi", "lahore", "islamabad", "peshawer", "quetta"];
-var match = false;
+// var userinput = prompt("Enter city name");
+// var cityNames = ["karachi", "lahore", "islamabad", "peshawer", "quetta"];
+// var match = false;
 
-for (var i = 0; i < cityNames.length; i++) {
-    if(userinput == cityNames[i]){
-        match = true
-        alert("city found")
-        break
-    }
-}
-if(match == false){
-   alert("City not found") 
-}
+// for (var i = 0; i < cityNames.length; i++) {
+//     if(userinput == cityNames[i]){
+//         match = true
+//         alert("city found")
+//         break
+//     }
+// }
+// if(match == false){
+//    alert("City not found") 
+// }
 
 
